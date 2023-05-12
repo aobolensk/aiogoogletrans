cad70b0c84e64fdd9063aaffd56001d1b1ea9a3b
Arseniy Obolenskiy <gooddoog@student.su>
2023-05-12 22:20:49 +0300

    Bump httpx version to 0.24.0

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ def install():
         packages=find_packages(exclude=["docs", "tests"]),
         keywords="google translate translator",
         install_requires=[
-            "httpx[http2]==0.23.3",
+            "httpx[http2]==0.24.0",
         ],
         tests_require=[
             "pytest",
